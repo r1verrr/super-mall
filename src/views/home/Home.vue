06564da303de3d6323e474de7885526498945fb2
Song Miaoqiang <739026055@qq.com>
2019-12-02 10:30:06 +0800

    'super'

---
+++ src/views/home/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="home">
+      <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+      <home-swiper :banners="banners"/>
+      <RecommendView/>
+    </div>
+
+</template>
+
+<script>
+
+  import NavBar from 'components/common/navbar/NavBar'
+
+  import HomeSwiper from './childComps/HomeSwiper'
+  // import { getHomeMultidata} from "network/home"
+  import { getHomeMultidata} from "../../network/home"
+  import RecommendView from './childComps/RecommendView'
+  
+
+
+
+export default {
+  name:"home",
+  components: {
+    NavBar,
+    getHomeMultidata,
+    HomeSwiper,
+    RecommendView
+  },
+  data(){
+    return {
+       banners:[],
+       recommends:[]
+    }
+  },
+  created(){
+    //1.请求多个数据
+     getHomeMultidata().then(
+      res =>{
+        console.log(res);
+        this.banners = res.data.banners.list;
+        this.recommends = res.data.recommends.list;
+      }
+    )
+
+  }
+}
+</script>
+
+<style >
+.home-nav{
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="recommends-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+props :{
+  recommends :{
+    type:Array,
+    default(){
+      return [ ]
+    }
+  }
+}
+}
+</script>
+
+<style>
+.recommend{
+    display: flex;
+
+}
+.recommend-item{
+  flex:1;
+}
+</style>
